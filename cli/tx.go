be5fb48be2c080c1e17c83d79c340ef3317053fb
0xFE <mohit@muthanna.com>
2018-03-19 11:23:20 -0400

    Added missing file.

---
+++ cli/tx.go
@@ -0,0 +1,106 @@
+package cli
+
+import (
+	"encoding/json"
+
+	"github.com/0xfe/microstellar"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func (cli *CLI) buildTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tx [sign|submit] [base64-encoded string] --signers seed1,seed2...",
+		Short: "manage trustlines between accounts and assets",
+		Args:  cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) > 0 {
+				showError(logrus.Fields{"cmd": "tx"}, "unrecognized tx command: %s, expecting: sign|submit", args[0])
+				return
+			}
+		},
+	}
+
+	cmd.AddCommand(cli.buildTxSignCmd())
+	cmd.AddCommand(cli.buildTxSubmitCmd())
+
+	return cmd
+}
+
+func (cli *CLI) buildTxSignCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sign [base64-encoded transaction] --signers seed1,seed2...",
+		Short: "sign the supplied transaction (on the current network) with the given seeds (or accounts)",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			b64tx := args[0]
+
+			logFields := logrus.Fields{"cmd": "sign"}
+			signers, err := cmd.Flags().GetStringSlice("signers")
+
+			if err != nil {
+				cli.error(logFields, "can't get signers: %v", err)
+				return
+			}
+
+			if len(signers) < 1 {
+				cli.error(logFields, "need at least one seed in --signers")
+				return
+			}
+
+			var seeds []string
+
+			for _, signer := range signers {
+				seed, err := cli.ResolveAccount(logFields, signer, "seed")
+
+				if err != nil {
+					cli.error(logFields, "bad signer account: %v", signer)
+					return
+				}
+
+				if microstellar.ValidSeed(seed) != nil {
+					cli.error(logFields, "no seed found in %v", signer)
+					return
+				}
+
+				seeds = append(seeds, seed)
+			}
+
+			signedTx, err := cli.ms.SignTransaction(b64tx, seeds...)
+
+			if err != nil {
+				cli.error(logFields, "signing error: %v", err)
+				return
+			}
+
+			showSuccess(signedTx)
+		},
+	}
+
+	buildFlagsForTxOptions(cmd)
+	return cmd
+}
+
+func (cli *CLI) buildTxSubmitCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "submit [base64-encoded transaction]",
+		Short: "submit the supplied transaction to the current network",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			b64tx := args[0]
+
+			logFields := logrus.Fields{"cmd": "submit"}
+			resp, err := cli.ms.SubmitTransaction(b64tx)
+
+			if err != nil {
+				cli.error(logFields, "submit error: %v", microstellar.ErrorString(err))
+				return
+			}
+
+			respJSON, _ := json.MarshalIndent(*resp, "", "  ")
+			showSuccess(string(respJSON))
+		},
+	}
+
+	return cmd
+}
